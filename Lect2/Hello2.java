cc809cd9ad36c9e75227d12f0b67cd334d313d4f
unknown <blue20@F06-20.fasfg.local>
2016-02-01 11:32:58 +0000

    v0.1 - Initialisation of notes and first programs.

---
+++ Lect2/Hello2.java
@@ -0,0 +1,14 @@
+/**
+* The HelloWorldApp class implements an application that
+* simply prints "Hello World!" to standard output.
+*/
+class Hello2
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World"); // Display the string.
+		System.out.println(" ");
+		System.out.println("This is my 2nd Java program");
+		System.out.print("Test");
+	}
+}
